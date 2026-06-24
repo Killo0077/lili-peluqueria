acf060890dd47132749ddabc14cbef83c869aac8
Killo007 <pablobaenav@gmail.com>
2026-06-24 17:00:43 +0100

    Add Github Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
-// @ts-check
+// // @ts-check
+// import { defineConfig } from 'astro/config';
+
+// import tailwindcss from '@tailwindcss/vite';
+
+// // https://astro.build/config
+// export default defineConfig({
+//   vite: {
+//     plugins: [tailwindcss()]
+//   }
+// });
+
+
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  site: 'https://killo0077.github.io',
+  base: '/lili-peluqueria',
 });
